825a10e6e3aa531509c4bfc1cf770134a77019c1
Gavri Kepets <gkepets@gmail.com>
2017-02-12 12:29:24 -0500

    Initial Commit

---
+++ City.java
@@ -0,0 +1,43 @@
+public class City {
+	
+	double rain; 
+	double snow; 
+	double heatWave; 
+	double hail; 
+	double tsunami; 
+	double drought; 
+	double volcano; 
+	double flooding; 
+	double hurricane; 
+	double tornado;
+	
+	
+	public City(double rain, double snow, double heatWave, double hail, double tsunami, double drought, double volcano, double flooding, double hurricane, double tornado){
+		this.rain = rain;
+		this.snow = snow;
+		this.heatWave = heatWave;
+		this.hail = hail;
+		this.tsunami = tsunami;
+		this.drought = drought;
+		this.volcano = volcano;
+		this.flooding = flooding;
+		this.hurricane = hurricane;
+		this.tornado = tornado;
+		this.climate = c;
+	}	
+
+public double[] getNumbers(double[] a){
+	a = {rain, 
+	 snow,
+	 heatWave, 
+	 hail,
+	 tsunami, 
+	 drought, 
+	 volcano,
+	 flooding, 
+	 hurricane; 
+	 tornado
+}
+	return a;
+}
+}
