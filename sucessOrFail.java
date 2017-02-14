d45f028f1fc35abc857ff56ea79d40e66433b2de
GIP2000 <gpress2222@gmail.com>
2017-02-14 16:35:10 -0500

    More Bug fixes

--- sucessOrFail.java
+++ sucessOrFail.java
@@ -1,21 +1,21 @@
-public class sucessOrFail extends numGenerator 
+public class sucessOrFail //extends numGenerator 
 {
  private double currentCheck; // set equal to the amount he made before this turn. 
  private double actual;
  private boolean outcome; 
  private double userInput;
 
- public sucessOrFail(WeatherMan wm, double check, City weather, double UserInput) 
+ public sucessOrFail(WeatherMan wm, double check, City weather, double UserInput, double getPercent) 
  {
   
-  super(weather); // wait to add the climate class to compile. 
+  //super(weather); // wait to add the climate class to compile. 
   this.userInput = UserInput;
   currentCheck = check; 
   System.out.println("Current Check: "+currentCheck);
   actual = 10.0*(Math.random() * (10-1) + 1);
   double raise;
-  System.out.println("Actual: "+actual + " getPercent "+ super.getPercent());
-  double aDif = (super.getPercent()-actual);
+  System.out.println("Actual: "+actual + " getPercent "+ getPercent);
+  double aDif = (getPercent-actual);
 
   if(aDif < 0) outcome = false; 
   else if (aDif >= 0 ) outcome = true; 
@@ -24,7 +24,7 @@ public class sucessOrFail extends numGenerator
   
   //raise = Math.abs(userInput - actual)/100.0; 
 
-  raise = UserInput - (actual / super.getPercent());
+  raise = UserInput - (actual / getPercent);
   // UI = 60% actual 70% and getPercent is 80%, then outcome = false, so raise = 
   // 60 - (70/80) = 78.5;
   //^^^ This works for some reason, i guess.
